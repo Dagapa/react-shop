9c56c9995d24cf45ab92c26450ca2c58f424d5cb
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-01-26 22:27:58 -0500

    add axios and call api products

--- shop/src/containers/ProductList.jsx
+++ shop/src/containers/ProductList.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
+import axios from 'axios';
+
+const API = 'http://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios(API);
+				setProducts(response.data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetchData();
+	}, []);
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => (
+					<ProductItem key={product.id}/>
+				))}
 			</div>
 		</section>
 	);
