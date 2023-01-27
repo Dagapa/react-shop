4cf33b9b3b7b330932935b9426e7a57be317edfc
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-01-26 22:10:21 -0500

    useState Toggle

--- shop/src/containers/Layout.jsx
+++ shop/src/containers/Layout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from '../components/Header'
 
 const Layout = ({ children }) => {
   return (
     <div className="Layout">
+      <Header/>
       {children}
       <Outlet />
     </div>
