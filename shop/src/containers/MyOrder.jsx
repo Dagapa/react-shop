cd55160e9471e56ea80cdaa7c6e3d48ee5cbfb34
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-01-30 18:58:06 -0500

    sum prices to cart

--- shop/src/containers/MyOrder.jsx
+++ shop/src/containers/MyOrder.jsx
@@ -7,28 +7,37 @@ import arrow from '../assets/icons/flechita.svg'
 
 const MyOrder = () => {
 	const { state } = useContext(AppContext);
-	return (
-		<aside className="MyOrder">
-			<div className="title-container">
-				<img src={arrow} alt="arrow" />
-				<p className="title">My order</p>
+
+	const sumTotal = () => {
+		const reducer = (acumulator, currentValue) =>
+			acumulator + currentValue.price;
+		const sum = state.cart.reduce(reducer, 0)
+		return sum;
+	}
+
+
+return (
+	<aside className="MyOrder">
+		<div className="title-container">
+			<img src={arrow} alt="arrow" />
+			<p className="title">My order</p>
+		</div>
+		<div className="my-order-content">
+			{state.cart.map(product => (
+				<OrderItem product={product} key={`orderItem-${product.id}`} />
+			))}
+			<div className="order">
+				<p>
+					<span>Total</span>
+				</p>
+				<p>${sumTotal()}</p>
 			</div>
-			<div className="my-order-content">
-				{state.cart.map(product => (
-					<OrderItem product={product} key={`orderItem-${product.id}`} />
-				))}
-				<div className="order">
-					<p>
-						<span>Total</span>
-					</p>
-					<p>$560.00</p>
-				</div>
-				<button className="primary-button">
-					Checkout
-				</button>
-			</div>
-		</aside>
-	);
+			<button className="primary-button">
+				Checkout
+			</button>
+		</div>
+	</aside>
+);
 }
 
 export default MyOrder;
